9ef4380f75c783aa5fa7f881bfc864e0f422117e
Erty Seidel <ertysdl+github@gmail.com>
2015-02-28 13:53:30 -0500

    WIP Sources

--- api/sources.php
+++ api/sources.php
@@ -2,6 +2,10 @@
 
 	$dbh = new PDO('mysql:host=localhost;dbname=ertyse5_rplists', 'ertyse5_rplists', '4rB4Qt7SNZD2WeLK');
 
+	if (isset($_POST['source'])) {
+
+	}
+
 	$sth = $dbh->prepare('SELECT * from sources WHERE approved = 1');
 	$sth->execute();
 	echo(json_encode($sth->fetchAll(PDO::FETCH_CLASS)));
